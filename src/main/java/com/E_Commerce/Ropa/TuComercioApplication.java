e298543b324fd154d9b8f55981454247f6849595
lucianocannici <luchocannici@gmail.com>
2024-08-06 22:05:51 -0300

    primer commit

---
+++ src/main/java/com/E_Commerce/Ropa/TuComercioApplication.java
@@ -0,0 +1,13 @@
+package com.E_Commerce.Ropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuComercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuComercioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/E_Commerce/Ropa/TuComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_Commerce.Ropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

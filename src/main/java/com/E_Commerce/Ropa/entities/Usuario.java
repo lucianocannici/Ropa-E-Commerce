1442700cd62a626a4cd89516952285724797b9a2
arthurcp96 <84679797+arthurcp96@users.noreply.github.com>
2024-08-12 00:57:52 -0300

    entidad pedido y usuario, se establece relacion many to one en pedido. Falta carrito y producto para correcta implementacion

---
+++ src/main/java/com/E_Commerce/Ropa/entities/Usuario.java
@@ -0,0 +1,30 @@
+package com.E_Commerce.Ropa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable= false)
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private String password;
+    private String telefono;
+    
+
+}
